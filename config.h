bc8bf0ee73885fcdfb6d06f64b42912c72fea78c
jkr1sh <jkrish.xyz@gmail.com>
2022-01-22 11:32:19 +0530

    Updated Config

--- config.h
+++ config.h
@@ -5,15 +5,15 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
+static unsigned int borderpx  = 2;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 8;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 8;       /* vert inner gap between windows */
-static unsigned int gappoh    = 8;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 8;       /* vert outer gap between windows and screen edge */
-static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 1;        /* 0 means no bar */
+static unsigned int gappih    = 0;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 0;       /* vert inner gap between windows */
+static unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
+static int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static int showbar            = 0;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "FreeMono:size=8", "JoyPixels:pixelsize=8:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
