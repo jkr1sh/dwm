546beb5191caccf4e4aae839db2c0eaec7622161
jkr1sh <jkrish.xyz@gmail.com>
2022-02-20 11:46:54 +0530

    upd build

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static unsigned int gappiv    = 0;       /* vert inner gap between windows */
 static unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 0;        /* 0 means no bar */
+static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "FreeMono:size=8", "JoyPixels:pixelsize=8:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
